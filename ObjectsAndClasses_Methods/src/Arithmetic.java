bb5317f0e5ebc39590412d78ad16e1ccb8953f31
Aleksandra Khamenok <sleza.shaitana.1@gmail.com>
2023-08-01 20:40:17 +0300

    homework 3 done

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,31 @@
 public class Arithmetic {
+    public int a;
+    public int b;
+    public int sum;
+    public int sum1;
+
+    public Arithmetic(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int addition() {
+        sum1 = a + b;
+        return sum1;
+    }
+    public int productOfNumber() {
+        sum = a * b;
+        return sum;
+    }
+    public int maxCount() {
+        if (a > b) {
+            return a;
+        } return b;
+    }
+    public int minCount() {
+        if (a < b) {
+            return a;
+        } return b;
+    }
 
 }
