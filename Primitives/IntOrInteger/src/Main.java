a8b5ca0bf6cb04baa96900e896c2c526820bf31b
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2023-10-02 00:50:24 +0300

    homework 2. Alphabet

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -4,6 +4,21 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+        for (char i = 'А'; i <= 'я'; i ++) {
+                int c = i;
+                System.out.println(c + " - " + i);
+        }
+
+        char lowercaseLetterYo = 'ё';
+        char uppercaseLetterYo = 'Ё';
+
+        int lowercaseLetterYoCode = lowercaseLetterYo;
+        int uppercaseLetterYoCode = uppercaseLetterYo;
+        System.out.println(lowercaseLetterYoCode + " - " + lowercaseLetterYo);
+        System.out.println( uppercaseLetterYoCode + " - " + uppercaseLetterYo);
+
+
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
