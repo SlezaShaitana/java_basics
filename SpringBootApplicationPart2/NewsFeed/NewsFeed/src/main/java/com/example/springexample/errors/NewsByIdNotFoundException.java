9d467a2c12dcc916e103ed41100e5681511d2c7a
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-04-11 14:35:19 +0300

    remove folder NewsFeed

--- SpringBootApplicationPart2/NewsFeed/NewsFeed/src/main/java/com/example/springexample/errors/NewsByIdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springexample.errors;
-
-public class NewsByIdNotFoundException extends RuntimeException {
-    public NewsByIdNotFoundException(String message) {
-        super(message);
-    }
-}
